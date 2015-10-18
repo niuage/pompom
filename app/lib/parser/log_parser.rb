926142f09c9050402cbc69f30466b5fa8f069e2c
Robin Boutros <niuage@gmail.com>
2015-10-18 00:50:33 -0400

    renaming some keys

--- app/lib/parser/log_parser.rb
+++ app/lib/parser/log_parser.rb
@@ -9,15 +9,20 @@ class Parser::LogParser < Parslet::Parser
   rule(:debug_client_id) { str("[DEBUG Client ") >> number >> str("]") >> space? }
 
   rule(:channel?) { (str("#") | str("$")).maybe }
-  rule(:user_prompt) { channel? >> match["^:"].repeat(1).as(:username) >> str(":") >> space? }
+  rule(:user_prompt) { channel? >> match["^:"].repeat(1).as(:character) >> str(":") >> space? }
 
   rule(:map_name) { (match["a-zA-Z\s"] >> start_options.absent?).repeat(1) }
 
+  # 2 choices:
+  # - have the same names as the DB fields here, and then use subtree(:params)
+  # in the Transform class
+  # - add .maybe everywhere, so that the options always show up, and I can be
+  # specific in the Transform class
   rule(:start_options) do
-    (str("q") >> space? >> number.as(:quality)) |
+    (str("q") >> space? >> number.as(:item_quantity)) |
     (str("ps") >> space? >> number.as(:pack_size)) |
-    (str("t") >> space? >> number.as(:tiers)) |
-    (str("name") >> space? >> map_name.as(:map_name))
+    (str("t") >> space? >> number.as(:tier)) |
+    (str("name") >> space? >> map_name.as(:name))
   end
 
   rule(:map_start) { str("ms:") >> (space? >> start_options).repeat(0) }
